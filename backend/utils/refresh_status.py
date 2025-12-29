761d2772e862d28740690abbd3ac8c2b186e8268
matteorosato <matteorosato.dev@gmail.com>
2025-12-29 17:55:02 +0100

    feat: removed (old) unused files

--- backend/utils/refresh_status.py
+++
@@ -1,9 +0,0 @@
-# Global status variable
-_refresh_status = "idle"  # idle, running, failed
-
-def set_refresh_status(value: str):
-    global _refresh_status
-    _refresh_status = value
-    
-def get_refresh_status() -> str:
-    return _refresh_status
